70fa8abca5aba0ac1c4f5ee2437d62ef78d92099
Vadim <nigma.corp@gmail.com>
2018-11-25 05:21:34 +0500

    First commit

---
+++ JS/script.js
@@ -0,0 +1,32 @@
+function myFirstApp(name, age) {
+	alert("Привет, меня зовут " + name + "  и это моя первая программа!");
+	function showSkills() {
+		let skills = [
+			"Знаю на базовом уровне HTML/CSS.",
+			"Умею создавать консольные приложения на Python.",
+			"Умею создавать консольные приложения на С++"
+		];
+
+		for (let i = 0; i < skill.length; i++) {
+			// Я владею HTML <br>
+			document.write()
+		}
+	}
+	showSkills();
+
+	function checkAge(){
+		if (age > 18) {
+			// Сообщение
+		} else {
+			//
+		}
+	}
+
+	function calcPow(num) {
+		// Напиши функционал
+	}
+
+	calcPow(4);
+}
+
+myFirstApp("Vadim", 35)
