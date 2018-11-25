fc8f8c8b5d4844be8ac839f0dbe82eb8494341b8
Vadim <nigma.corp@gmail.com>
2018-11-25 09:29:22 +0500

    First application

--- JS/script.js
+++ JS/script.js
@@ -1,32 +1,39 @@
 function myFirstApp(name, age) {
-	alert("Привет, меня зовут " + name + "  и это моя первая программа!");
+	alert("Hello, my name is " + name + ". This is my first program!");
 	function showSkills() {
 		let skills = [
-			"Знаю на базовом уровне HTML/CSS.",
-			"Умею создавать консольные приложения на Python.",
-			"Умею создавать консольные приложения на С++"
+			"I know on base level HTML/CSS.",
+			"I can create console applications on Python.",
+			"I can create console applications on C++"
 		];
 
-		for (let i = 0; i < skill.length; i++) {
+		for (let i = 0; i < skills.length; i++) {
 			// Я владею HTML <br>
-			document.write()
+			document.write(skills[i] + "<br>");
 		}
 	}
+	
 	showSkills();
 
 	function checkAge(){
 		if (age > 18) {
-			// Сообщение
+			document.write("You are adult!<br>");
 		} else {
-			//
+			document.write("You are young!<br>");
 		}
 	}
+	
+	checkAge();
 
 	function calcPow(num) {
-		// Напиши функционал
+		let p = 1;
+		for (let i = 1; i <= num; i++) {
+			p = p * num;
+		}
+		document.write(" Answer " + num + "^2 = " + p + "<br>");
 	}
 
 	calcPow(4);
 }
 
-myFirstApp("Vadim", 35)
+myFirstApp("Vadim", 35);
